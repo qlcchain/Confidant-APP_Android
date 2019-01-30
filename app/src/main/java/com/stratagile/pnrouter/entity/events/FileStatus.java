877c0605c72c2fe703d8978f8f12094afc4f0074
winqandroid <1144515262@qq.com>
2019-01-30 16:49:29 +0800

    filestatus

--- app/src/main/java/com/stratagile/pnrouter/entity/events/FileStatus.java
+++ app/src/main/java/com/stratagile/pnrouter/entity/events/FileStatus.java
@@ -3,18 +3,32 @@ package com.stratagile.pnrouter.entity.events;
 public class FileStatus {
 
     String filePath;
+    long fileSize;
+    Boolean isDownLoad;
+    Boolean isComplete;
+    Boolean isStop;
+    int segSeqResult;
+    int segSeqTotal;
+    int speed;
+    Boolean SendGgain;
     int result;  // 0成功，1文件不存在
-    public FileStatus()
-    {
-
-    }
     public FileStatus(String filePath,int result)
     {
-
+          this.filePath = filePath;
+          this.result = result;
     }
-    public FileStatus(String path ,long fileSize, Boolean isDownLoad, Boolean isComplete, Boolean isStop, int segSeqResult, int segSeqTotal, int speed,Boolean SendGgain,int result)
+    public FileStatus(String filePath ,long fileSize, Boolean isDownLoad, Boolean isComplete, Boolean isStop, int segSeqResult, int segSeqTotal, int speed,Boolean SendGgain,int result)
     {
-
+        this.filePath = filePath;
+        this.fileSize = fileSize;
+        this.isDownLoad = isDownLoad;
+        this.isComplete = isComplete;
+        this.isStop = isStop;
+        this.segSeqResult = segSeqResult;
+        this.segSeqTotal = segSeqTotal;
+        this.speed = speed;
+        this.SendGgain = SendGgain;
+        this.result = result;
     }
     public String getFilePath() {
         return filePath;
