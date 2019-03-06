b8d7dbe2e752cf10db8c875eb3630586061fffe9
369457358@qq.com <huzhipeng0826>
2019-03-06 18:23:08 +0800

    修改好友，草稿，自动登录

--- app/src/main/java/com/stratagile/pnrouter/data/service/BackGroundService.java
+++ app/src/main/java/com/stratagile/pnrouter/data/service/BackGroundService.java
@@ -16,11 +16,13 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 
+import com.socks.library.KLog;
 import com.stratagile.pnrouter.R;
 import com.stratagile.pnrouter.application.AppConfig;
 import com.stratagile.pnrouter.constant.ConstantValue;
 import com.stratagile.pnrouter.ui.activity.main.MainActivity;
 import com.stratagile.pnrouter.utils.OkHttpUtils;
+import com.xiaomi.mipush.sdk.MiPushClient;
 
 import org.json.JSONException;
 
@@ -42,7 +44,7 @@ import static android.support.v4.app.NotificationCompat.PRIORITY_LOW;
  */
 
 public class BackGroundService extends Service {
-    Notification notification;
+    NotificationCompat.Builder notification;
     private Context mContext;
     private MediaPlayer bgmediaPlayer;
     private boolean isrun = true;
@@ -72,7 +74,8 @@ public class BackGroundService extends Service {
                 notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         //1.通知栏占用，不清楚的看官网或者音乐类APP的效果
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            notification = new Notification.Builder(mContext).setChannelId(CHANNEL_ONE_ID)
+            notification = new NotificationCompat.Builder(mContext)
+                    .setChannelId(CHANNEL_ONE_ID)
                     .setSmallIcon(R.drawable.mipush_small_notification)
                     .setLargeIcon(BitmapFactory.decodeResource(Resources.getSystem(),R.mipmap.ic_launcher))
                     .setWhen(System.currentTimeMillis())
@@ -82,73 +85,27 @@ public class BackGroundService extends Service {
                     .setOngoing(false)
                     .setPriority(PRIORITY_DEFAULT)
                     .setAutoCancel(false)
+                    .setWhen(0)
                     .setSound(null)
                     .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                    .setVisibility(Notification.VISIBILITY_PRIVATE)
-                    .build();
+                    .setVisibility(Notification.VISIBILITY_PRIVATE);
         }else{
-            notification = new Notification.Builder(mContext)
+           notification = new NotificationCompat.Builder(mContext)
                     .setSmallIcon(R.drawable.mipush_small_notification)
                     .setLargeIcon(BitmapFactory.decodeResource(Resources.getSystem(),R.mipmap.ic_launcher))
-                    .setWhen(System.currentTimeMillis())
                     .setTicker(AppConfig.instance.getString(R.string.app_name))
                     .setContentTitle(AppConfig.instance.getString(R.string.app_name))
                     .setContentText(AppConfig.instance.getString(R.string.MessageRetrievalService_background_connection_enabled))
                     .setOngoing(false)
                     .setPriority(PRIORITY_DEFAULT)
+                    .setWhen(0)
                     .setAutoCancel(false)
-                    .setSound(null,null)
-                    .build();
+                    .setSound(null);
         }
         /*使用startForeground,如果id为0，那么notification将不会显示*/
-        startForeground(313399, notification);
-        //2.开启线程（或者需要定时操作的事情）
-      /*  new Thread() {
-            @Override
-            public void run() {
-                super.run();
-                while (isrun) {
-
-                    //你需要执行的任务
-                    try {
-                        Thread.sleep(10000);
-                    } catch (InterruptedException es) {
-                        es.printStackTrace();
-                    }
-                    HashMap<String, String> map  =  new HashMap();
-                    OkHttpUtils.getInstance().doPost(ConstantValue.INSTANCE.getPushURL()+"test",map,new OkHttpUtils.OkCallback(){
-
-                        @Override
-                        public void onFailure(Exception e) {
-
-                        }
-
-                        @Override
-                        public void onResponse(String json){
-
-                        }
-                    });
-
-                }
-                //进行自己的操作
-            }
-        }.start();*/
-        //3.最关键的神来之笔，也是最投机的动作，没办法要骗过CPU
-        //这就是播放音乐类APP不被杀的做法，自己找个无声MP3放进来循环播放
-        /*if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            if (bgmediaPlayer == null) {
-               bgmediaPlayer = MediaPlayer.create(this, R.raw.silent);
-                bgmediaPlayer.setLooping(true);
-                bgmediaPlayer.start();
-            }
-        }else{
-            if (bgmediaPlayer == null) {
-                bgmediaPlayer = MediaPlayer.create(this, R.raw.silent);
-                bgmediaPlayer.setLooping(true);
-                bgmediaPlayer.start();
-            }
-        }*/
-
+        startForeground(313399, notification.build());
+        KLog.i("ppm 注册小米推送。。");
+        MiPushClient.registerPush(this, AppConfig.instance.getMI_PUSH_APP_ID(), AppConfig.instance.getMI_PUSH_APP_KEY());
         return START_STICKY;
     }
 
