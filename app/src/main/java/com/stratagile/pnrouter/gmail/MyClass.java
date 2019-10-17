20095ad2861d572747d2f0daf55d6178da9daed7
winqandroid <1144515262@qq.com>
2019-10-17 15:06:55 +0800

    google api

--- app/src/main/java/com/stratagile/pnrouter/gmail/MyClass.java
+++ app/src/main/java/com/stratagile/pnrouter/gmail/MyClass.java
@@ -2,7 +2,24 @@ package com.stratagile.pnrouter.gmail;
 
 import com.google.api.services.gmail.Gmail;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+
+// ...
+
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.ListMessagesResponse;
+import com.google.api.services.gmail.model.Message;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import qlc.utils.Base64.Base64;
+
 
 // ...
 
@@ -11,21 +28,48 @@ public class MyClass {
     // ...
 
 
+
     /**
-     * Immediately and permanently deletes the specified thread. This operation cannot
-     * be undone. Prefer threads.trash instead.
+     * Send an email from the user's mailbox to its recipient.
      *
      * @param service Authorized Gmail API instance.
      * @param userId User's email address. The special value "me"
      * can be used to indicate the authenticated user.
-     * @param threadId ID of Thread to delete.
+     * @param emailContent Email to be sent.
+     * @return The sent message
+     * @throws MessagingException
      * @throws IOException
      */
-    public static void deleteThread(Gmail service, String userId, String threadId)
-            throws IOException {
-       /* service.users().threads().delete(userId, threadId).execute();*/
-        System.out.println("Thread with id: " + threadId + " deleted successfully.");
+    public static Message sendMessage(Gmail service,
+                                      String userId,
+                                      MimeMessage emailContent)
+            throws MessagingException, IOException {
+        Message message = createMessageWithEmail(emailContent);
+        message = service.users().messages().send(userId, message).execute();
+
+        System.out.println("Message id: " + message.getId());
+        return message;
     }
 
+    /**
+     * Create a message from an email.
+     *
+     * @param emailContent Email to be set to raw of message
+     * @return a message containing a base64url encoded email
+     * @throws IOException
+     * @throws MessagingException
+     */
+    public static Message createMessageWithEmail(MimeMessage emailContent)
+            throws MessagingException, IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        emailContent.writeTo(buffer);
+        byte[] bytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+        return message;
+    }
     // ...
+
 }
+
