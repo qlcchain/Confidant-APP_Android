f5451d28bd11c2abc97447b417a165c238c4b98d
winqandroid <1144515262@qq.com>
2019-11-26 14:13:08 +0800

    时间显示优化

--- app/src/main/java/com/stratagile/pnrouter/utils/DateUtil.java
+++ app/src/main/java/com/stratagile/pnrouter/utils/DateUtil.java
@@ -7,7 +7,9 @@ import android.content.Context;
 import com.hyphenate.util.TimeInfo;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Locale;
 
 import static com.hyphenate.util.DateUtils.getTodayStartAndEndTime;
@@ -27,30 +29,18 @@ public class DateUtil {
         String var2 = Locale.getDefault().getLanguage();
         boolean var3 = var2.startsWith("zh");
         long var4 = var0.getTime();
-        if(isSameDay(var4)) {
+        if(isSameDay(var4)) {//同一天
             if (is24Time(context)) {
                 var1 = "HH:mm";
             } else {
                 var1 = "hh:mm aa";
             }
-        } else if(isYesterday(var4)) {
-            if (is24Time(context)) {
-                return (new SimpleDateFormat("HH:mm", Locale.ENGLISH)).format(var0) +" Yesterday" ;
-            } else {
-                return (new SimpleDateFormat("hh:mm aa", Locale.ENGLISH)).format(var0) +" Yesterday" ;
-            }
-        } else if(var3) {
-            if (is24Time(context)) {
-                var1 = "HH:mm MMM dd";
-            } else {
-                var1 = "hh:mm aa MMM dd";
-            }
-        } else {
-            if (is24Time(context)) {
-                var1 = "HH:mm MMM dd";
-            } else {
-                var1 = "hh:mm aa MMM dd";
-            }
+        }  else if(isSameWeek(var4) && isSameYear(var4) ) {//同一周且在同一年
+            var1 = "EEE";
+        } else if(isDifferentWeek(var4) && isSameYear(var4) ) {//不在同一周，但在同一年
+            var1 = "dd MMM";
+        }else if(var3) {//不在同一年
+            var1 = "dd/MM/yyyy";
         }
 
         return (new SimpleDateFormat(var1, Locale.ENGLISH)).format(var0);
@@ -159,5 +149,61 @@ public class DateUtil {
         Date date = new Date(milSecond);
         SimpleDateFormat format = new SimpleDateFormat(pattern);
         return format.format(date);
+    }
+
+    /**
+     * start
+     * 本周开始时间戳
+     */
+    public static long getWeekStartTime() {
+        Calendar cal = Calendar.getInstance();
+        // 获取星期日开始时间戳
+        cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTimeInMillis();
+    }
+    //获取本年的开始时间
+    public static long getBeginDayOfYear() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, getNowYear());
+        cal.set(Calendar.MONTH, Calendar.JANUARY);
+        cal.set(Calendar.DATE, 1);
+        return getDayStartTime(cal.getTime());
+    }
+    //获取今年是哪一年
+    public static Integer getNowYear() {
+        Date date = new Date();
+        GregorianCalendar gc = (GregorianCalendar) Calendar.getInstance();
+        gc.setTime(date);
+        return Integer.valueOf(gc.get(Integer.valueOf(1)));
+    }
+    //获取某个日期的开始时间
+    public static long getDayStartTime(Date d) {
+        Calendar calendar = Calendar.getInstance();
+        if(null != d){
+            calendar.setTime(d);
+        }
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTimeInMillis();
+    }
+    private static boolean isSameWeek(long var0) {
+        long var2 =  getWeekStartTime();
+        return var0 >= var2;
+    }
+    private static boolean isDifferentWeek(long var0) {
+        long var2 =  getWeekStartTime();
+        return var0 < var2;
+    }
+    private static boolean isSameYear(long var0) {
+        long var2 =  getBeginDayOfYear();
+        return var0 >= var2;
+    }
+    private static boolean isDifferentYear(long var0) {
+        long var2 =  getBeginDayOfYear();
+        return var0 < var2;
     }
 }
