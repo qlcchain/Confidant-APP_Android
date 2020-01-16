dbbd36df4fc5de2be9d9e9adc0bc03730bdbe7d4
winqandroid <1144515262@qq.com>
2020-01-16 21:53:49 +0800

    线程未启动

--- app/src/main/java/com/stratagile/pnrouter/ui/activity/encryption/ContactsEncryptionActivity.kt
+++ app/src/main/java/com/stratagile/pnrouter/ui/activity/encryption/ContactsEncryptionActivity.kt
@@ -469,7 +469,7 @@ class ContactsEncryptionActivity : BaseActivity(), ContactsEncryptionContract.Vi
                                                 isUpload = false;
                                                 getNodeData(false);
                                             }
-                                        })
+                                        }).start()
 
                                     }
                                     .show()
