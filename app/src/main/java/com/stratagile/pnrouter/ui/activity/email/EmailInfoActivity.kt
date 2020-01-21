dcf532b8b0ccc4048046fe8d48baacda191824b5
zhanglang108 <zhanglang108@sina.com>
2020-01-21 18:24:52 +0800

    备份完成更新icon

--- app/src/main/java/com/stratagile/pnrouter/ui/activity/email/EmailInfoActivity.kt
+++ app/src/main/java/com/stratagile/pnrouter/ui/activity/email/EmailInfoActivity.kt
@@ -179,8 +179,10 @@ class EmailInfoActivity : BaseActivity(), EmailInfoContract.View , PNRouterServi
             runOnUiThread {
                 closeProgressDialog()
                 toast(R.string.success)
+                var drawable = getResources().getDrawable(R.mipmap.statusbar_download_node_backups)
+                drawable.setBounds(0, 0, 72, 72);
+                backMenu.setCompoundDrawables(drawable, null, null,null);
             }
-
         }else if(jBakupEmailRsp.params.retCode == 1)
         {
             runOnUiThread {
