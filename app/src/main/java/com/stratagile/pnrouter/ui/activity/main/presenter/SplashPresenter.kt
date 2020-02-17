47f6ac145ade59c55b3be36bcffd1c1cce6b337d
zhanglang108 <zhanglang108@sina.com>
2020-02-17 14:43:48 +0800

    去掉定位

--- app/src/main/java/com/stratagile/pnrouter/ui/activity/main/presenter/SplashPresenter.kt
+++ app/src/main/java/com/stratagile/pnrouter/ui/activity/main/presenter/SplashPresenter.kt
@@ -78,8 +78,7 @@ constructor(internal var httpAPIWrapper: HttpAPIWrapper, private val mView: Spla
                 .requestCode(101)
                 .permission(
                         Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                        Manifest.permission.ACCESS_FINE_LOCATION
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
                 )
                 .callback(permission)
                 .start()
